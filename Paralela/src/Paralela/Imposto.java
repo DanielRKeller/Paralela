34ec9a7a383136710cb059ce643dd64a6e6ef80e
Daniel <danielkeller820@gmail.com>
2019-05-23 22:59:27 -0300

    first commit

---
+++ Paralela/src/Paralela/Imposto.java
@@ -0,0 +1,10 @@
+package Paralela;
+
+public class Imposto {
+    
+    public static void main(String[]args){
+    
+    calculoimp imposto  = new imp();
+    imposto.setVisible(true);
+    } 
+}
---
+++ Paralela/src/Paralela/imp.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paralela;
+
+/**
+ *
+ * @author 2017315377
+ */
+class imp extends calculoimp {
+
+    public imp() {
+    }
+    
+}
